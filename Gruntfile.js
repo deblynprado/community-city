217901e0fc15677f06922c5a688bab7da239d452
Deblyn Prado <deblynprado@gmail.com>
2015-08-25 17:22:07 -0300

    Adding grunt to generate final zip - close #1

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+	'use strict';
+	var gruntConfig = {
+		pkg: grunt.file.readJSON('package.json'),      
+    compress: {
+      main: {
+        options: {
+          archive: '<%= pkg.name %>.zip'
+        },
+        files: [
+        {src:['**', '!*.zip', '!*.git', '!*.gitignore', '!*.DS_Store', '!*.md', '!*.json', '!Gruntfile.js', '!node_modules/**', '!original/**', '!PSDs/**', '!sass/**']}
+      ]
+    }
+  }
+
+};
+
+grunt.initConfig(gruntConfig);
+
+var keys = Object.keys(gruntConfig);
+var tasks = [];
+
+for(var i = 1, l = keys.length; i < l; i++) {
+  tasks.push(keys[i]);
+}
+grunt.loadNpmTasks('grunt-contrib-compress');
+grunt.registerTask('default', tasks);
+
+};
