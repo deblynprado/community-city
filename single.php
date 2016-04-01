88864d043e660a01d2596bc7bb81e4b4ff06b9e2
Leo Baiano <ljunior2005@gmail.com>
2016-04-01 12:45:24 -0300

    add comment_form in single.php #12

--- single.php
+++ single.php
@@ -11,9 +11,12 @@
 				</p>
 			</header>
 			<div class="content" itemprop="articleBody">
-				<?php the_content(); ?>
+				<?php
+					the_content();
+					the_tags( '<ul class="tags"><li>' . __( 'Tags', 'commcitytheme' ) . ':</li><li>', '</li><li>', '</li></ul>' );
+					comment_form();
+				?>
 			</div>
-			<?php the_tags( '<ul class="tags"><li>' . __( 'Tags', 'commcitytheme' ) . ':</li><li>', '</li><li>', '</li></ul>' ); ?>
 		</article>
 	<?php endwhile; ?>
 	<!-- post navigation -->
